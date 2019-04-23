ba102a87106a2940616f365c73faddd7dd75f096
SphereII <kilpatrick.jeff@gmail.com>
2019-04-23 16:09:52 -0300

    Update NerdPole.cs

--- AntiNerdPole/Scripts/NerdPole.cs
+++ AntiNerdPole/Scripts/NerdPole.cs
@@ -2,12 +2,30 @@ using System;
 
 public class AntiNerdPoll : SkyManager
 {
-    // returns true if the nerd poll check fails.
+    // returns true if the block cannot be placed.
     public static bool NerdPoleCheck( EntityAlive entity )
     {
         if(entity is EntityPlayerLocal)
         {
-            if(!entity.onGround && !(entity as EntityPlayerLocal).isAttachedToLadder)
+            // If you are in God mode, let you do whatever.
+            if((entity as EntityPlayerLocal).IsGodMode == true)
+                return false;
+
+            if((entity as EntityPlayerLocal).IsFlyMode == true)
+                return false;
+
+            if((entity as EntityPlayerLocal).IsInElevator())
+                return false;
+
+            if((entity as EntityPlayerLocal).IsInWater() )
+                return false;
+        
+            // If you are on a ladder, let you place blocks.
+            if((entity as EntityPlayerLocal).isAttachedToLadder)
+                return false;
+
+            // If you aren't on the ground, don't place the block.
+            if(!entity.onGround )
                 return true;
         }
         return false;
